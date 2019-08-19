1989e53f5162e37eb4e4086ebbeb663658dc8b76
Sebastian <sebskelton@gmail.com>
2019-08-20 11:47:52 +1200

    Badass game

---
+++ New Unity Project/New Unity Project/Assets/scripts/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed;
+    public float clampValue;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePaddle(Input.GetAxis("Horizontal"));
+//        Debug.Log(Input.GetAxis("vertical"));
+//        gameObject.transform.position += new Vector3(0, 0, Input.GetAxis("vertical"));
+    }
+
+    public void MovePaddle(float inputAxisInfo) 
+    {
+        Vector3 clampedGameObjectPosition 
+            = gameObject.transform.position
+            + new Vector3(0, 0, inputAxisInfo * speed);
+
+        clampedGameObjectPosition.z = Mathf.Clamp(clampedGameObjectPosition.z, -clampValue, clampValue);
+
+        gameObject.transform.position = clampedGameObjectPosition;
+
+
+    }
+
+}
